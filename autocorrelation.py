4ddeab9a0b92b2ba2503dd039c5cdcb12b0414cc
Xin Yao <yaoxyx@gmail.com>
2018-11-22 17:26:50 +0800

    update code

--- autocorrelation.py
+++ autocorrelation.py
@@ -98,7 +98,7 @@ def get_flows_from_file(filename, column_num, minSpeed = 2, maxSpeed = 150):
 
 
 if __name__ == '__main__':
-    flows_co, flows_z = get_sim_flows()
-    #flows_co, flows_z = get_flows_from_file('./data/sj_051316_1km.csv', 30)
+    #flows_co, flows_z = get_sim_flows()
+    flows_co, flows_z = get_flows_from_file('./data/sj_051316_1km.csv', 30)
     moran_i = flow_autocorrelation(flows_co, flows_z)
     print(moran_i)
