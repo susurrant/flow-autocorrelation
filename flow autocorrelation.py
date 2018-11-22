2b6cc2bf7400ec5f05e60b737ce908a63ad6f46f
Xin Yao <susurrant@outlook.com>
2018-11-22 10:33:22 +0800

    update codes

--- flow autocorrelation.py
+++ flow autocorrelation.py
@@ -92,7 +92,35 @@ def flow_autocorrelation(vectors, standardization=False):
     return moran_i
 
 
+def get_flows_from_file(filename, column_num, minSpeed = 2, maxSpeed = 150):
+    si = {}
+
+    with open(filename, 'r') as f:
+        f.readline()
+        while True:
+            line1 = f.readline().strip()
+            if line1:
+                sl1 = line1.split(',')
+                sl2 = f.readline().strip().split(',')
+                if sl1[1] == '1' and minSpeed < float(sl1[-2]) < maxSpeed:
+                    ogid = int(sl1[-1])
+                    dgid = int(sl2[-1])
+                    if (ogid, dgid) not in si:
+                        si[(ogid, dgid)] = 0
+                    si[(ogid, dgid)] += 1
+            else:
+                break
+
+    flows = []
+    for k, v in si.items():
+        oy, ox, dy, dx = k[0] // column_num, k[0] % column_num, k[1] // column_num, k[1] % column_num
+        flows.append(Vector([ox, oy], [dx, dy], v))
+    print(len(flows))
+    return flows
+
+
 if __name__ == '__main__':
-    flows = get_sim_flows()
+    #flows = get_sim_flows()
+    flows = get_flows_from_file('./data/sj_051316_1km.csv', 30)
     moran_i = flow_autocorrelation(flows)
     print(moran_i)
